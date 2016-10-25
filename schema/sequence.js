41f373d0b977f85a85ddbefa79a40bb62b7cad00
Kazuma Fujimaki <fujimaki-k@fujimakishouten.com>
2016-10-25 19:54:03 +0900

    Initial commit

---
+++ schema/sequence.js
@@ -0,0 +1,30 @@
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+/* eslint-env es6, mocha, node */
+/* eslint-extends: eslint:recommended */
+'use strict';
+
+
+
+// Export module
+module.exports = {
+    key: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    value: {
+        type: Number,
+        default: 0
+    }
+};
+
+
+
+/*
+ * Local variables:
+ * tab-width: 4
+ * c-basic-offset: 4
+ * c-hanging-comment-ender-p: nil
+ * End:
+ */
+
